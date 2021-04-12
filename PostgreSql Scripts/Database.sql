66343833036ad671451b4ad6139382889ff52c4b
OrionProgrammer <asheenk@gmail.com>
2021-04-12 16:28:44 +0200

    Added database scripts

---
+++ PostgreSql Scripts/Database.sql
@@ -0,0 +1,12 @@
+-- Database: Unit Converter
+
+-- DROP DATABASE "Unit Converter";
+
+CREATE DATABASE "Unit Converter"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
